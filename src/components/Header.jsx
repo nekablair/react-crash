5ea8a9402b7e231d201be4b0bb9ee843f6baa1b7
Neka Blair <ourblairfamily@gmail.com>
2024-03-02 08:49:24 -0600

    shortcut rce

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-
-// rcc shortcut to bring in the component as a class instead of a function
-export default class Header extends Component {
+//rce shortcut creates class component and export default at the bottom
+export class Header extends Component {
   render() {
     return (
       <div>Header</div>
     )
   }
 }
+
+export default Header
