6acaa2eb67024038ddd1c9f57b5ba29183550db0
Neka Blair <ourblairfamily@gmail.com>
2024-03-02 08:47:58 -0600

    change

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,13 @@ import './App.css'
 function App() {
   // components can functions or classes
   const [count, setCount] = useState(0)
-  const name = "Brad"
-  const x = false
 
   return (
     // looks like html, but it's JSX
     //can only have one parent element, hence <> </>
     <>
       <div className='container'>
-        <h1>Hello {x ? "Yes": "No"}</h1>
+        <h1>Hello From React</h1>
       </div>
         
     </>
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+// rcc shortcut to bring in the component as a class instead of a function
+export default class Header extends Component {
+  render() {
+    return (
+      <div>Header</div>
+    )
+  }
+}
