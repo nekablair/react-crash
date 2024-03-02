30747041bc2105245f28f8a5f8b7f9684617f1e8
Neka Blair <ourblairfamily@gmail.com>
2024-03-02 08:50:22 -0600

    rafce shortcut

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,9 @@
-import React, { Component } from 'react'
-//rce shortcut creates class component and export default at the bottom
-export class Header extends Component {
-  render() {
-    return (
-      <div>Header</div>
-    )
-  }
+import React from 'react'
+//rafce arrow function exports at bottom
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
 }
 
 export default Header
