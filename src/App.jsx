896d298e2a886308874c65976b9cd4f704d40d8b
Neka Blair <ourblairfamily@gmail.com>
2024-03-02 08:30:53 -0600

    add JS

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ function App() {
   // components can functions or classes
   const [count, setCount] = useState(0)
   const name = "Brad"
+  const x = true
 
   return (
     // looks like html, but it's JSX
     //can only have one parent element, hence <> </>
     <>
       <div className='container'>
-        <h1>Hello {name}</h1>
+        <h1>Hello {x ? "Yes": "No"}</h1>
       </div>
         
     </>
