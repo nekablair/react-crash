76efef06caabeaca445da3292d7c09831453bc7e
Neka Blair <ourblairfamily@gmail.com>
2024-03-02 22:03:26 -0600

    add tasks in array of objects

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Tasks = () => {
+
+    const tasks = [
+        {
+            id: 1,
+            text: 'Grocery Shopping',
+            day: 'Feb 5th at 2:30pm',
+            reminder: false
+        },
+        {
+            id: 2,
+            text: 'Practice React',
+            day: 'Feb 12th at 7:00am',
+            reminder: true
+        },
+        {
+            id: 3,
+            text: 'Birthday Party',
+            day: 'Feb 28th at 5:00pm',
+            reminder: false
+        }
+    ]
+
+  return (
+    <div>Tasks</div>
+  )
+}
+
+export default Tasks
