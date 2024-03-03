91078fa997074b1dacd972d0e653b4734eb9206a
Neka Blair <ourblairfamily@gmail.com>
2024-03-02 22:31:05 -0600

    destructuring app component state and using it as prop in task component

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,28 @@ import Header from './components/Header.jsx'
 import Tasks from './components/Tasks.jsx'
 
 function App() {
-  // components can functions or classes
-  const [count, setCount] = useState(0)
+  // now part of app component state, can now use a props so every other component has access to it
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Grocery Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false
+    },
+    {
+        id: 2,
+        text: 'Practice React',
+        day: 'Feb 12th at 7:00am',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Birthday Party',
+        day: 'Feb 28th at 5:00pm',
+        reminder: false
+    }
+])
 
   return (
     // looks like html, but it's JSX
@@ -13,7 +33,7 @@ function App() {
     <>
       <div className='container'>
         <Header />
-        <Tasks />
+        <Tasks  tasks={tasks}/>
       </div>
         
     </>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,32 +1,7 @@
 import React from 'react'
-import { useState } from 'react'
 
-const Tasks = () => {
-    //tasks variable, setTasks updates state or changes being made
-    //useState when inside a component is component state, it isn't coming from somewhere outside the component
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Grocery Shopping',
-            day: 'Feb 5th at 2:30pm',
-            reminder: false
-        },
-        {
-            id: 2,
-            text: 'Practice React',
-            day: 'Feb 12th at 7:00am',
-            reminder: true
-        },
-        {
-            id: 3,
-            text: 'Birthday Party',
-            day: 'Feb 28th at 5:00pm',
-            reminder: false
-        }
-    ])
-    //you can't change state, it's immutable, so trying to do tasks.push() won't work, it rerenders everytime a change is made, it's recreated and sent down, it one way data
-    //to change you do the following
-    // setTasks([...tasks, {}])//can change state with setTasks and using spread operator to get everything in tasks, you can set it to something new
+//          destructuring
+const Tasks = ({ tasks }) => { 
 
   return (
     <>
