ca555f92495c18f1fbee77831f8d12794b905ee9
Neka Blair <ourblairfamily@gmail.com>
2024-03-03 22:45:15 -0600

    add fa icons

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { FaTimes } from 'react'
 
 const Task = ({ task }) => {
   return (
     <>
         <div className = 'task'>
-            <h3>{task.text}</h3>
+            <h3>{task.text}<FaTimes style={color: 'red'} /></h3>
             <p>{task.day}</p>
         </div>
     </>
