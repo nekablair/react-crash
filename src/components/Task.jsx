8e347bb374139a02263578eb6ed29eeb54c353b5
Neka Blair <ourblairfamily@gmail.com>
2024-03-03 22:29:35 -0600

    add task prop to Task and return to screen via props

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Task = () => {
+const Task = ({ task }) => {
   return (
     <>
         <div className = 'task'>
-            <h3>My Task</h3>
+            <h3>{task.text}</h3>
         </div>
     </>
   )
