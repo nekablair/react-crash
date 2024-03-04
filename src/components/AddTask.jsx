e76c68cfad2fe09b66b295562a440ad2495b4b54
Neka Blair <ourblairfamily@gmail.com>
2024-03-04 14:25:09 -0600

    update onchange function with only currentTarget in order to update the checkbox state to true onclick

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -21,7 +21,7 @@ const AddTask = () => {
             </div>
             <div className='form-control form-control-check'>
                 <label>Set Reminder</label>
-                <input type='checkbox' value={reminder} onChange={(e) => setReminder(e.target.currentTarget.checked)} />
+                <input type='checkbox' value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)} />
                 
             </div>
             <input type="submit" value='Save Task' className='btn btn-block' />
