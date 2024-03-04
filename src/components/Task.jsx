baffa6674ddbce4f21e30cfdc2565c47dd9c66b1
Neka Blair <ourblairfamily@gmail.com>
2024-03-04 08:22:21 -0600

    add fa-react styling

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { FaTimes } from 'react'
+//using icons by bringing them in like react components
+import { FaTimes } from 'react-icons/fa'
 
 const Task = ({ task }) => {
   return (
     <>
         <div className = 'task'>
-            <h3>{task.text}<FaTimes style={color: 'red'} /></h3>
+            <h3>{task.text}<FaTimes style={{color: 'red', cursor: 'pointer'}} /></h3>
             <p>{task.day}</p>
         </div>
     </>
