5d5957356cb4da05dc5b41342eaa7f809b581b06
Neka Blair <ourblairfamily@gmail.com>
2024-03-06 08:19:27 -0600

    add mock db

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,7 @@ import AddTask from './components/AddTask.jsx'
 function App() {
   // now part of app component state, can now use a props so every other component has access to it
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Grocery Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false
-    },
-    {
-        id: 2,
-        text: 'Practice React',
-        day: 'Feb 12th at 7:00am',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'Birthday Party',
-        day: 'Feb 28th at 5:00pm',
-        reminder: false
-    }
-])
+  const [tasks, setTasks] = useState([])
 
 //add task
   const addTask = (task) => {
